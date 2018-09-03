93c64a149b65bbaa96f9d33a1195c263f100ab4c
Mlle DR <mllelafortune@gmail.com>
2018-09-03 14:06:38 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@
 
 var S_Store = [];
 
-
 function setup() {
   createCanvas(640, 640);
-
+  background(0);
+  
   var npoints = 90;
   for (var i = 0; i < npoints; i++) {
     S_Store[i] = new SPINNER(width / 2, height / 2, (i / (npoints / 2)) * Math.PI);
@@ -14,24 +14,18 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-
   for (var i = 0; i < S_Store.length; i++) {
     S_Store[i].show();
   }
 }
 
-
 function SPINNER(cX, cY, angle) {
-
   this.posmin = createVector(0, 0); // min point
   this.posmax = createVector(0, 0); // max point
 
   this.show = function () {
-
     this.posmin.x = pX(cX, 50, angle);
     this.posmin.y = pY(cY, 50, angle);
-
     this.posmax.x = pX(cX, 200, angle);
     this.posmax.y = pY(cY, 200, angle);
 
@@ -42,10 +36,8 @@ function SPINNER(cX, cY, angle) {
     strokeWeight(3);
     stroke(153, 136, 136, 150);
     point(this.posmax.x, this.posmax.y);
-
   }  
 }
-
 
 // cX and cY are the starting points (ex: center of canvas)
 function pX(cX, radius, angle) {
