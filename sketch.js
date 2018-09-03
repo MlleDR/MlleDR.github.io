2e15ec0e7b580450e7613b0f86b474c6fab9c0ac
Mlle DR <mllelafortune@gmail.com>
2018-09-03 13:55:31 +0200

    First commit
    id #01 : Creates a min and max circle. x and y starting point, radius and angle required.

---
+++ sketch.js
@@ -0,0 +1,57 @@
+// 03.09.18 : first commit
+// to do : update function to add animation
+
+var S_Store = [];
+
+
+function setup() {
+  createCanvas(640, 640);
+
+  var npoints = 90;
+  for (var i = 0; i < npoints; i++) {
+    S_Store[i] = new SPINNER(width / 2, height / 2, (i / (npoints / 2)) * Math.PI);
+  }
+}
+
+function draw() {
+  background(0);
+
+  for (var i = 0; i < S_Store.length; i++) {
+    S_Store[i].show();
+  }
+}
+
+
+function SPINNER(cX, cY, angle) {
+
+  this.posmin = createVector(0, 0); // min point
+  this.posmax = createVector(0, 0); // max point
+
+  this.show = function () {
+
+    this.posmin.x = pX(cX, 50, angle);
+    this.posmin.y = pY(cY, 50, angle);
+
+    this.posmax.x = pX(cX, 200, angle);
+    this.posmax.y = pY(cY, 200, angle);
+
+    strokeWeight(1);
+    stroke(154, 26, 64, 150);
+    point(this.posmin.x, this.posmin.y);
+
+    strokeWeight(3);
+    stroke(153, 136, 136, 150);
+    point(this.posmax.x, this.posmax.y);
+
+  }  
+}
+
+
+// cX and cY are the starting points (ex: center of canvas)
+function pX(cX, radius, angle) {
+  return cX + radius * Math.sin(angle);
+}
+
+function pY(cY, radius, angle) {
+  return cY + radius * Math.cos(angle);
+}
