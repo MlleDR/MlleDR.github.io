eaceb95d52f10e8222c94cc17b2294ea5b361a9f
Mlle DR <mllelafortune@gmail.com>
2018-09-03 15:03:25 +0200

    added center point

--- sketch.js
+++ sketch.js
@@ -9,27 +9,32 @@ function setup() {
 }
 
 function draw() {
-   S_Store[0].show();
+  S_Store[0].show();
 }
 
 function SPINNER(cX, cY) {
+  this.poscenter = createVector(0, 0); // center point
   this.posmin = createVector(0, 0); // min point
   this.posmax = createVector(0, 0); // max point
 
   this.show = function () {
+    this.poscenter.x = pX(cX, 0, 0);
+    this.poscenter.y = pY(cY, 0, 0);
     this.posmin.x = pX(cX, 50, 90);
     this.posmin.y = pY(cY, 50, 90);
     this.posmax.x = pX(cX, 200, 180);
     this.posmax.y = pY(cY, 200, 180);
-    
-    strokeWeight(1);
-    stroke(154, 26, 64, 150);
-    point(this.posmin.x, this.posmin.y);
 
     strokeWeight(3);
-    stroke(153, 136, 136, 150);
+    stroke(255);
+    point(this.poscenter.x, this.poscenter.y);
+
+    stroke(255, 0, 0);
+    point(this.posmin.x, this.posmin.y);
+
+    stroke(0, 255, 255);
     point(this.posmax.x, this.posmax.y);
-  }  
+  }
 }
 
 // cX and cY are the starting points (ex: center of canvas)
